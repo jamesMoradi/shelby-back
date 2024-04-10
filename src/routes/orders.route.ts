a5d09e164dae62738449ace33a9319be5441d59f
james moradi <james.js.dev@outlook.com>
2024-04-10 16:58:38 +0430

    debigged 01

--- src/routes/orders.route.ts
+++ src/routes/orders.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createItem, deleteItem, getOrders, updateItem } from "../controllers/items.controller";
 import multer from "multer";
+import {upload as storage} from '../configs/multer.config'
 
 const route = Router()
 
 route.get('/orders', getOrders)
 route.patch('/orders/:name', updateItem)
 route.delete('/orders/:name', deleteItem)
-route.post('/orders', multer({dest : 'uploads/'}).single('image') ,createItem)
+route.post('/orders', storage.single('image') ,createItem)
 
 export default route
